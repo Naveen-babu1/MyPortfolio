0edbd4d95bd3f8614d0919eb6a54f9eaee48d904
Naveen-babu1 <babu.na@northeastern.edu>
2023-12-19 17:17:09 -0500

    navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,9 @@
-
+import Navbar from "../navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Row className="nav">
+        <Col className="logo" xs={8}>
+          NAVEEN BABU
+        </Col>
+        <Col>
+          <div className="main-col-2-card-4">
+            <div className="location-pin">.</div>
+            <h5>Boston, MA</h5>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Navbar;
